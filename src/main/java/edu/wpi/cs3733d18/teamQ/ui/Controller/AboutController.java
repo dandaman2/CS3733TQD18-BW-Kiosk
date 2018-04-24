7e5f5bb11a005e3e75d3a18807c2c8040fd153c5
saraharm <saarmstrong@wpi.edu>
2018-04-24 00:48:13 -0400

    Merge remote-tracking branch 'remotes/origin/dev' into dev
    # Conflicts:
    #	src/main/java/edu/wpi/cs3733d18/teamQ/ui/Controller/AboutController.java

--- src/main/java/edu/wpi/cs3733d18/teamQ/ui/Controller/AboutController.java
+++ src/main/java/edu/wpi/cs3733d18/teamQ/ui/Controller/AboutController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
@@ -25,8 +26,8 @@ public class AboutController {
 //       label.setPadding(noPadding);
        Label label2 = new Label("Credit");
 //       label2.setPadding(noPadding);
-       label.setStyle("-fx-font-size: 27pt;");
-       label2.setStyle("-fx-font-size: 27pt;");
+       label.setFont(Font.font("Georgia", 30));
+       label2.setFont(Font.font("Georgia", 30));
        BorderPane layout = new BorderPane(); // Implements a Border Pane (splits screen into 5 sections)
        HBox hb = addHBox();
        hb.setStyle("-fx-background-color: #012D5A;");
@@ -40,16 +41,11 @@ public class AboutController {
        layout.setBottom(hb2);
 
        Label teamName = new Label("The Quenched Quinotaurs:");
-//       teamName.setPadding(noPadding);
-       teamName.setStyle("-fx-font-size: 15pt;");
+       teamName.setFont(Font.font("Georgia", 20));
        Label teamMembers = new Label("Lead Software Engineer: Dan Duff");
-//       teamMembers.setPadding(noPadding);
        Label teamMembers1 = new Label("Algorithms Lead: Nugzar Chkhaidze");
-//       teamMembers1.setPadding(noPadding);
-       Label teamMembers2 = new Label("Database Lead - James Kenney");
-//       teamMembers2.setPadding(noPadding);
+       Label teamMembers2 = new Label("Database Lead: James Kenney");
        Label teamMembers3 = new Label("Assistant Lead Software Engineers:");
-//       teamMembers3.setPadding(noPadding);
        Label teamMembers4 = new Label("Norman Delorey, Andrew DeRusha, Jesse d'Almeida, Aleksander Ibro");
        Label teamMembers5 = new Label("Scrum Master: Maggie Goodwin");
        Label teamMembers6 = new Label("Product Owner: Andrew DeRusha");
@@ -62,24 +58,35 @@ public class AboutController {
 
        //teamMembers.setStyle("-fx-font-size: 12pt;");
        teamMembers.setTextAlignment(TextAlignment.LEFT);
+       teamMembers.setFont(Font.font("Georgia", 15));
+       teamMembers1.setFont(Font.font("Georgia", 15));
+       teamMembers2.setFont(Font.font("Georgia", 15));
+       teamMembers3.setFont(Font.font("Georgia", 15));
+       teamMembers4.setFont(Font.font("Georgia", 15));
+       teamMembers5.setFont(Font.font("Georgia", 15));
+       teamMembers6.setFont(Font.font("Georgia", 15));
+       teamMembers7.setFont(Font.font("Georgia", 15));
+       teamMembers8.setFont(Font.font("Georgia", 15));
+       teamMembers9.setFont(Font.font("Georgia", 15));
+       teamMembers10.setFont(Font.font("Georgia", 15));
+       teamMembers11.setFont(Font.font("Georgia", 15));
+       teamMembers12.setFont(Font.font("Georgia", 15));
 
        Label thanksTitle = new Label("Special Thanks:");
-       thanksTitle.setStyle("-fx-font-size: 15pt;");
+       thanksTitle.setFont(Font.font("Georgia", 20));
 
-       Label thanksText = new Label("Thank you to Brigham and Women’s Faulkner Hospital and Andrew Shinn for their time and input\n" +
-               "on this application.");
-       thanksText.setStyle("-fx-font-size: 12pt;");
+       Label thanksText = new Label("Thank you to Brigham and Women’s Faulkner Hospital and Andrew Shinn for their time and input on this application.");
+       thanksText.setFont(Font.font("Georgia", 15));
        thanksText.setTextAlignment(TextAlignment.CENTER);
 
-       Label copyState = new Label("The Brigham & Women’s Hospital maps and data used in this application are copyrighted and provided for the sole\n" +
-               "use of educational purposes.");
-       copyState.setStyle("-fx-font-size: 10pt;");
+       Label copyState = new Label("The Brigham & Women’s Hospital maps and data used in this application are copyrighted and provided for the sole use of educational purposes.");
+       copyState.setFont(Font.font("Georgia", 12));
        copyState.setTextAlignment(TextAlignment.CENTER);
 
        Label apiName = new Label("Team APIs Used:");
-       apiName.setStyle("-fx-font-size: 15pt;");
        Label apiExName = new Label("External APIs Used:");
-       apiExName.setStyle("-fx-font-size: 15pt;");
+       apiName.setFont(Font.font("Georgia", 20));
+       apiExName.setFont(Font.font("Georgia", 20));
        Label teamApiUsed = new Label("Team O's Gift service request API");
        Label apiUsed = new Label("Yahoo Weather Java API");
        Label apiUsed1 = new Label("Jfoenix");
@@ -95,9 +102,21 @@ public class AboutController {
        Label apiUsed11 = new Label("Jbcrypt");
        Label apiUsed12 = new Label("Slf4j-api");
        Label apiUsed13 = new Label("Slf4j-simple");
-       //apiUsed.setStyle("-fx-font-size: 12pt;");
-       //teamApiUsed.setStyle("-fx-font-size: 12pt;");
-       apiUsed.setTextAlignment(TextAlignment.LEFT);
+       teamApiUsed.setFont(Font.font("Georgia", 15));
+       apiUsed.setFont(Font.font("Georgia", 15));
+       apiUsed1.setFont(Font.font("Georgia", 15));
+       apiUsed2.setFont(Font.font("Georgia", 15));
+       apiUsed3.setFont(Font.font("Georgia", 15));
+       apiUsed4.setFont(Font.font("Georgia", 15));
+       apiUsed5.setFont(Font.font("Georgia", 15));
+       apiUsed6.setFont(Font.font("Georgia", 15));
+       apiUsed7.setFont(Font.font("Georgia", 15));
+       apiUsed8.setFont(Font.font("Georgia", 15));
+       apiUsed9.setFont(Font.font("Georgia", 15));
+       apiUsed10.setFont(Font.font("Georgia", 15));
+       apiUsed11.setFont(Font.font("Georgia", 15));
+       apiUsed12.setFont(Font.font("Georgia", 15));
+       apiUsed13.setFont(Font.font("Georgia", 15));
 
        // Formatting for the admin login
        GridPane grid = new GridPane();
@@ -136,37 +155,38 @@ public class AboutController {
        //grid.add(reg3, 0, 15);
        //grid.add(reg4, 0, 16);
        grid.add(thanksTitle, 0, 18);
-       grid.setColumnSpan(thanksTitle,2);
+       grid.setColumnSpan(thanksTitle,3);
        GridPane.setHalignment(thanksTitle, HPos.CENTER);
        grid.add(thanksText, 0, 19);
-       grid.setColumnSpan(thanksText,2);
+       grid.setColumnSpan(thanksText,3);
        GridPane.setHalignment(thanksText, HPos.CENTER);
 
        //grid.add(reg5, 0, 19);
        //grid.add(reg6, 0, 20);
        grid.add(copyState, 0, 21);
-       grid.setColumnSpan(copyState,2);
+       grid.setColumnSpan(copyState,3);
        GridPane.setHalignment(copyState, HPos.CENTER);
 
        // Filling a grid pane that holds the username and password fields
        grid.add(label2, 1, 0);
        grid.add(apiName, 1, 1);
        grid.add(teamApiUsed, 1, 2);
-       grid.add(apiExName, 1, 3);
-       grid.add(apiUsed, 1, 4);
-       grid.add(apiUsed1, 1, 5);
-       grid.add(apiUsed2, 1, 6);
-       grid.add(apiUsed3, 1, 7);
-       grid.add(apiUsed4, 1, 8);
-       grid.add(apiUsed5, 1, 9);
-       grid.add(apiUsed6, 1, 10);
-       grid.add(apiUsed7, 1, 11);
-       grid.add(apiUsed8, 1, 12);
-       grid.add(apiUsed9, 1, 13);
-       grid.add(apiUsed10, 1, 14);
-       grid.add(apiUsed11, 1, 15);
-       grid.add(apiUsed12, 1, 16);
-       grid.add(apiUsed13, 1, 17);
+       grid.add(reg5, 1, 3);
+       grid.add(apiExName, 1, 4);
+       grid.add(apiUsed, 1, 5);
+       grid.add(apiUsed1, 1, 6);
+       grid.add(apiUsed2, 1, 7);
+       grid.add(apiUsed3, 1, 8);
+       grid.add(apiUsed4, 1, 9);
+       grid.add(apiUsed5, 1, 10);
+       grid.add(apiUsed6, 1, 11);
+       grid.add(apiUsed7, 1, 12);
+       grid.add(apiUsed8, 1, 13);
+       grid.add(apiUsed9, 1, 14);
+       grid.add(apiUsed10, 2, 5);
+       grid.add(apiUsed11, 2, 6);
+       grid.add(apiUsed12, 2, 7);
+       grid.add(apiUsed13, 2, 8);
 
        // Filling a Vbox that holds the main message the grid and the submit button
        vb.getChildren().addAll(grid, aPane);
