a63502f1fb36d6a81a6147083ab6a7487507b948
James Kenney <jkenney320@gmail.com>
2018-04-25 01:31:45 -0400

    gift requests stored in db

--- src/main/java/edu/wpi/cs3733d18/teamQ/ui/Requests/GiftRequest.java
+++ src/main/java/edu/wpi/cs3733d18/teamQ/ui/Requests/GiftRequest.java
@@ -6,18 +6,26 @@ import java.util.GregorianCalendar;
 public class GiftRequest extends Request{
     public GiftRequest(int room, int fulfilled, String whoFulfilled, String email, String phoneNumber, String requestIDStr, GregorianCalendar date) {
         super(room, fulfilled, whoFulfilled, email, phoneNumber, requestIDStr, date);
+        this.setPriority("Medium");
+        this.setType("Gift");
     }
 
     public GiftRequest(String firstName, String lastName, String roomName, int fulfilled, String whoFulfilled, String email, String phoneNumber, String requestIDStr, GregorianCalendar date, String priority) {
         super(firstName, lastName, roomName, fulfilled, whoFulfilled, email, phoneNumber, requestIDStr, date, priority);
+        this.setPriority("Medium");
+        this.setType("Gift");
     }
 
     public GiftRequest(int room, String email, String phoneNumber, String requestIDStr) {
         super(room, email, phoneNumber, requestIDStr);
+        this.setPriority("Medium");
+        this.setType("Gift");
     }
 
     public GiftRequest(String firstName, String lastName, String email, String phoneNumber, String roomName) {
         super(firstName, lastName, email, phoneNumber, roomName);
+        this.setPriority("Medium");
+        this.setType("Gift");
     }
 
     @Override
