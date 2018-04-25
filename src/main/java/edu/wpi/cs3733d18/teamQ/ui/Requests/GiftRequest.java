2d3682464cc72c99ce177abee55fe54564e9146d
James Kenney <jkenney320@gmail.com>
2018-04-25 00:57:45 -0400

    adding giftRequest stats

---
+++ src/main/java/edu/wpi/cs3733d18/teamQ/ui/Requests/GiftRequest.java
@@ -0,0 +1,41 @@
+package edu.wpi.cs3733d18.teamQ.ui.Requests;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class GiftRequest extends Request{
+    public GiftRequest(int room, int fulfilled, String whoFulfilled, String email, String phoneNumber, String requestIDStr, GregorianCalendar date) {
+        super(room, fulfilled, whoFulfilled, email, phoneNumber, requestIDStr, date);
+    }
+
+    public GiftRequest(String firstName, String lastName, String roomName, int fulfilled, String whoFulfilled, String email, String phoneNumber, String requestIDStr, GregorianCalendar date, String priority) {
+        super(firstName, lastName, roomName, fulfilled, whoFulfilled, email, phoneNumber, requestIDStr, date, priority);
+    }
+
+    public GiftRequest(int room, String email, String phoneNumber, String requestIDStr) {
+        super(room, email, phoneNumber, requestIDStr);
+    }
+
+    public GiftRequest(String firstName, String lastName, String email, String phoneNumber, String roomName) {
+        super(firstName, lastName, email, phoneNumber, roomName);
+    }
+
+    @Override
+    public String getDetails() {
+        return null;
+    }
+
+    @Override
+    public String getSpecifics() {
+        return "Gift Request";
+    }
+
+    @Override
+    public ArrayList<Request> addRequest(ArrayList<Request> arrayList) {
+        arrayList.add(this);
+
+        System.out.println("Sent!");
+
+        return arrayList;
+    }
+}
